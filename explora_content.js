5dd6decf255214f6f4f6ddeb9ecef1b577b06f0f
m-chele <michele.cenere@gmail.com>
2017-10-04 15:10:46 +0200

    riesco a colorare di verde lo sfondo del div contenente le timbrature

--- explora_content.js
+++ explora_content.js
@@ -1,36 +1,43 @@
 chrome.runtime.onMessage.addListener (
     function(request, sender, sendResponse) {
-
+      // $("span.Title").css("background-color","blue");
       var workedMilliseconds = 0;
-      $("#cartTable tr").each(function(){
-        var checkinSpan = $(this).find("span.entrata");
-        var checkoutSpan = $(this).find("span.uscita");
-        var validSpanCount = checkinSpan.size() + checkoutSpan.size();
+      $('div')
+        .filter(function() {
+          return this.id.match(/_Grid1_25_3_viewDiv/);
+        }).css("background-color","green");
+      // $("div:regex(id, *_Grid1_25_3_viewDiv)").css("background-color","red");
+      // $("span select[title='Entrata']").css("background-color","red");
+      // $("#pzvuf_Grid1_25_3_viewDiv").each(function(){
+        // alert($("select[title='Entrata']"));
 
-        if(validSpanCount > 0 && validSpanCount != 4)
-        {
-          $(this).css("background-color","red");
-        }
-
-        // Exclude:
-        //    days with missing checks
-        //    day with more then 2 checkin and 2 checkout
-        if(validSpanCount == 4)
-        {
-          var morningCheckin = getMillisecondsFromText($(checkinSpan.get(0)).text());
-          var morningCheckout =  getMillisecondsFromText($(checkoutSpan.get(0)).text());
-
-          var afternoonCheckin =  getMillisecondsFromText($(checkinSpan.get(1)).text());
-          var afternoonCheckout = getMillisecondsFromText($(checkoutSpan.get(1)).text());
-
-
-          workedMilliseconds += (morningCheckout - morningCheckin + afternoonCheckout - afternoonCheckin);
-
-        }
-
-      });
-        sendResponse(workedTime(workedMilliseconds));
-        return true;
+    //     var checkinSpan = $(this).find("span.entrata");
+    //     var checkoutSpan = $(this).find("span.uscita");
+    //     var validSpanCount = checkinSpan.size() + checkoutSpan.size();
+    //
+    //     if(validSpanCount > 0 && validSpanCount != 4)
+    //     {
+    //       $(this).css("background-color","red");
+    //     }
+    //
+    //     // Exclude:
+    //     //    days with missing checks
+    //     //    day with more then 2 checkin and 2 checkout
+    //     if(validSpanCount == 4)
+    //     {
+    //       var morningCheckin = getMillisecondsFromText($(checkinSpan.get(0)).text());
+    //       var morningCheckout =  getMillisecondsFromText($(checkoutSpan.get(0)).text());
+    //
+    //       var afternoonCheckin =  getMillisecondsFromText($(checkinSpan.get(1)).text());
+    //       var afternoonCheckout = getMillisecondsFromText($(checkoutSpan.get(1)).text());
+    //
+    //       workedMilliseconds += (morningCheckout - morningCheckin + afternoonCheckout - afternoonCheckin);
+    //
+    //     }
+    //
+      // });
+    //     sendResponse(workedTime(workedMilliseconds));
+    //     return true;
     }
 );
 
